9679301d340ca2a0aa62e8d4c6886580f4340fba
tzhpfro1 <roger.pfaeffli@swisscom.com>
2023-11-13 21:34:59 +0100

    Add view helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,16 @@
 module MoviesHelper
+
+  def total_gross(movie)
+    # used in movies index view
+    if movie.flop?
+      # calls the custom flop? method out of movie model
+      "Flop!"
+    else
+    number_to_currency(movie.total_gross, precision: 0)
+    end
+  end
+
+  def year_of(movie)
+    movie.released_on.year
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+  def flop?
+    # helper method flop? (returns true or false) to define
+    # if a movie is a flop or not
+    total_gross < 225000000 || total_gross.blank?
+  end
 end
