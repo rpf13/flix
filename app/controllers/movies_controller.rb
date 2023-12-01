8f218e7fedc246dbaddf26a6d1dc0e9ac73a60fc
tzhpfro1 <roger.pfaeffli@swisscom.com>
2023-12-01 10:17:29 +0100

    Add the create form and functionality

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,11 +13,30 @@ class MoviesController < ApplicationController
 
   def update
     @movie = Movie.find(params[:id])
-    movie_params = params.require(:movie).
-      permit(:title, :description, :rating, :released_on, :total_gross)
-
     @movie.update(movie_params)
     redirect_to @movie
-    
   end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    @movie.save
+    redirect_to @movie
+  end
+
+
 end
+
+
+private
+# Private methods can't be called from outside of the class
+# to follow DRY, method for hand over params is created and can
+# be re-used in update, create, .. method
+
+  def movie_params
+    params.require(:movie).
+      permit(:title, :description, :rating, :released_on, :total_gross)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
 
   root "movies#index"
-  get "movies" => "movies#index"
-  get "movies/:id" => "movies#show", as: "movie"
-  get "movies/:id/edit" => "movies#edit", as: "edit_movie"
-  patch "movies/:id/" => "movies#update"
+
+  resources :movies
+
+  # Above resources conventional shortcut, removes the need to create
+  # individual routes, as they are shown below
+  
+  # get "movies" => "movies#index"
+  # get "movies/:id" => "movies#show", as: "movie"
+  # get "movies/:id/edit" => "movies#edit", as: "edit_movie"
+  # patch "movies/:id/" => "movies#update"
 
 end
