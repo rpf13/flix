2465efc9be1b08f82edb17e6a4696e2d8fe44275
tzhpfro1 <roger.pfaeffli@swisscom.com>
2023-12-04 13:48:54 +0100

    Add custom query methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,9 @@
 class Movie < ApplicationRecord
+
+  def self.released
+    where("released_on < ?", Time.now).order("released_on desc")
+  end
+
   def flop?
     # helper method flop? (returns true or false) to define
     # if a movie is a flop or not
