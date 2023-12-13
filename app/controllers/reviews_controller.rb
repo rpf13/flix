5e01251089e32d613d5594d1be9a2f3e629cd1e5
tzhpfro1 <roger.pfaeffli@swisscom.com>
2023-12-13 10:04:25 +0100

    Add review site and functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,39 @@
 class ReviewsController < ApplicationController
+
+  before_action :set_movie
+  # We always want to run the set_movie method before
+  # running any action's code - we want to have the movie
+  # object
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
+
+  def new
+    @review = @movie.reviews.new
+  end
+
+  def create
+    # review_params comes from private method
+    @review = @movie.reviews.new(review_params)
+
+    if @review.save
+      redirect_to movie_reviews_path(@movie),
+      notice: "Thanks for your review!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+end
+
+private
+
+# The create action must send a list of attributes, define
+# which arae valid
+def review_params
+  params.require(:review).permit(:name, :comment, :stars)
+end
+
+def set_movie
+  @movie = Movie.find(params[:movie_id])
 end
