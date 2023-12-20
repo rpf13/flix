364ad4b7a47a1d9fc3b5938340f726fe68ce931e
tzhpfro1 <roger.pfaeffli@swisscom.com>
2023-12-20 15:51:21 +0100

    Add full CRUD functionality for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,31 @@ class UsersController < ApplicationController
     end
   end
 
-end
+  def edit
+    @user = User.find(params[:id])
+  end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user, notice: "Account successfully updated!"
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to movies_url, status: :see_other,
+      alert: "Account successfully deleted!"    
+  end
+
+end
 
 private
 
 def user_params
   params.require(:user).
-    permit(:name, :email, :password, :password_confirmation)
+    permit(:name, :username, :email, :password, :password_confirmation)
 end
