6f4b554f059937b6c0322c1d6fd2e4ef0cc1bb85
tzhpfro1 <roger.pfaeffli@swisscom.com>
2023-12-24 07:51:29 +0100

    Add session authentication part

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,21 @@ class SessionsController < ApplicationController
   end
 
   def create
-    fail
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      # check if user is true / exists and if so, pass the
+      # password out of the submitted form to the authenticate
+      # method, which will hash it and compare to the stored
+      session[:user_id] = user.id
+      # if user is authenticated, session id is created and we use
+      # the user id value and assign it to the user_id key of the
+      redirect_to user, notice: "Welcome back, #{user.name}!"
+    else
+      flash.now[:alert] = "invalid email/password combination!"
+      # flash.now makes the flash message available in the same 
+      # request rather than waiting for the next request.
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
